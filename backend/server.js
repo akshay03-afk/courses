540f1d9bb56baeefeea8bd11fb2a453bde31c7c9
akshay kumar <akshaykumarchai1@gmail.com>
2021-07-09 13:41:56 +0545

    Adding static file

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-const path = require("path");
 const express = require("express");
+const path = require("path");
 const courses = require("./data/courses")
 
 const app = express();
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 5000;
 
 
-app.use(express.static("public"))
+
 // app.get("/", (req, res) =>{
 //     console.log("Api is running");
 
@@ -19,14 +19,23 @@ app.get("/api/courses", (req, res) =>{
 });
 
 app.get("/api/courses/:id", (req, res) =>{
+    
     const course = courses.find(c => c.id === req.params.id);
+    
     res.json(course);
-});
+})
 
-
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname + "/public/build/index.html" ))
-)
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+  } else {
+    app.get('/', (req, res) => {
+      res.send('API is running....')
+    })
+  }
 
 app.listen(PORT || 5000, ()=>{
     console.log(`Server is running on port ${PORT}`)
