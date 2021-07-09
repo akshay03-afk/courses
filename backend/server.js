6336b035968e2bd25067a7f4867bf51918640b5a
akshay kumar <akshaykumarchai1@gmail.com>
2021-07-09 13:59:17 +0545

    Removing all the packages from backend

--- backend/server.js
+++ backend/server.js
@@ -25,17 +25,14 @@ app.get("/api/courses/:id", (req, res) =>{
     res.json(course);
 })
 
-if (process.env.NODE_ENV === 'production') {
+
+
     app.use(express.static(path.join(__dirname, '../frontend/build')))
   
     app.get('*', (req, res) =>
       res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     )
-  } else {
-    app.get('/', (req, res) => {
-      res.send('API is running....')
-    })
-  }
+  
 
 app.listen(PORT || 5000, ()=>{
     console.log(`Server is running on port ${PORT}`)
